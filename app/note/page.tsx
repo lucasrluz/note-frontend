e22e4a4cd82f0bc59d84ebe0fd1e0026e21f2eb1
lucasrluz <lucasr.luzbr@gmail.com>
2023-08-10 08:55:50 -0300

    feat: criado funções para fazer requisição para buscar notas do usuário

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -1,14 +1,40 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
 export default function IndexPage() {
-  function test(title: string) {
-    return (
-      <div className="flex h-12 flex-col items-center justify-center border-b border-stone-300">
-        <div className="w-60">
-          <button>
-            <h1 className="line-clamp-1">{title}</h1>
-          </button>
-        </div>
-      </div>
+  async function getNotes() {
+    const response = await axios.get(
+      "https://note-api-w041.onrender.com/note",
+      {
+        headers: { JWT: localStorage.getItem("jwt") },
+      },
     );
+
+    return response;
+  }
+
+  const [notes, setNotes]: any[] = React.useState([]);
+
+  useEffect(() => {
+    const notesData = async () => {
+      const notes = await getNotes();
+
+      setNotes(notes);
+    };
+
+    notesData().catch((error) => alert(error));
+  }, []);
+
+  function foo(e: any, element: any) {
+    const titleElement: HTMLInputElement = document.querySelector("#title")!;
+    const contentElement: HTMLInputElement =
+      document.querySelector("#content")!;
+
+    titleElement.value = element.title;
+    contentElement.value = element.content;
   }
 
   return (
@@ -32,21 +58,32 @@ export default function IndexPage() {
       </div>
       <div className="flex h-full flex-row">
         <div className="w-1/5 border-r border-stone-300">
-          {test("Título A B C D E F G H I J K L M N")}
-          {test("Título B")}
-          {test("Título C")}
+          {notes.data &&
+            notes.data.map((element: any) => (
+              <div className="flex h-12 flex-col items-center justify-center border-b border-stone-300">
+                <div className="w-60">
+                  <button
+                    onClick={(e) => {
+                      foo(e, element);
+                    }}
+                  >
+                    <h1 className="line-clamp-1">{element.title}</h1>
+                  </button>
+                </div>
+              </div>
+            ))}
         </div>
         <div className="flex w-4/5 flex-col">
           <textarea
             className="ml-3 mt-1 h-10 bg-transparent text-3xl font-bold focus:outline-none"
             name=""
-            id=""
+            id="title"
             placeholder="Título"
           ></textarea>
           <textarea
             className="ml-3 h-full bg-transparent focus:outline-none"
             name=""
-            id=""
+            id="content"
             placeholder="Conteúdo..."
           ></textarea>
         </div>
