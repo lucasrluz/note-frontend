5b03fe6603ff85e36accf95d11cd69b389ecb6b8
lucasrluz <lucasr.luzbr@gmail.com>
2023-08-10 14:24:43 -0300

    refactor: mudança para tamanho fixo dos componentes para editar nota

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import axios from "axios";
-import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 export default function IndexPage() {
@@ -40,14 +39,14 @@ export default function IndexPage() {
   return (
     <main className="flex h-screen flex-col">
       <div className="flex h-12 flex-row">
-        <div className="flex w-1/5 flex-col items-center justify-center border-b border-r border-stone-300">
+        <div className="flex w-80 flex-col items-center justify-center border-b border-r border-stone-300">
           <input
             className="w-60 rounded-md border border-solid border-gray-300 focus:outline-none"
             type="text"
             placeholder="Buscar nota"
           />
         </div>
-        <div className="flex w-4/5 flex-row items-center justify-end border-b border-stone-300">
+        <div className="flex w-full flex-row items-center justify-end border-b border-stone-300">
           <button className="mr-3 h-8 w-16 rounded bg-green-500 text-white hover:bg-green-400">
             Salvar
           </button>
@@ -57,7 +56,7 @@ export default function IndexPage() {
         </div>
       </div>
       <div className="flex h-full flex-row">
-        <div className="w-1/5 border-r border-stone-300">
+        <div className="w-80 border-r border-stone-300">
           {notes.data &&
             notes.data.map((element: any) => (
               <div className="flex h-12 flex-col items-center justify-center border-b border-stone-300">
@@ -73,7 +72,7 @@ export default function IndexPage() {
               </div>
             ))}
         </div>
-        <div className="flex w-4/5 flex-col">
+        <div className="flex w-full flex-col">
           <textarea
             className="ml-3 mt-1 h-10 bg-transparent text-3xl font-bold focus:outline-none"
             name=""
